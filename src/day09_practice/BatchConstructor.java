081f2ec6c38041bd6182afea83f300f6e5838dcc
Selcuk054 <107318967+Selcuk054@users.noreply.github.com>
2022-07-30 22:09:45 +0200

    30.07

---
+++ src/day09_practice/BatchConstructor.java
@@ -0,0 +1,16 @@
+package day09_practice;
+
+public class BatchConstructor {
+    public static void main(String[] args) {
+
+
+
+      Batch batch=new Batch();
+
+        System.out.println("batch.str3 = " + batch.str3);
+
+        batch.batc();
+
+
+    }
+}
