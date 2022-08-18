dc3e182618679c51251df43be12b47b0d1bd7427
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-08-18 18:45:25 +0200

    cakisma deneme1

--- src/UfukBranch_Calisma.java
+++ src/UfukBranch_Calisma.java
@@ -1,5 +1,9 @@
 public class UfukBranch_Calisma {
     public static void main(String[] args) {
         System.out.println("Branchte calisma 1");
+
+
+        System.out.println("Ufuk branch i ile cakismasi beklenen");
+
     }
 }
