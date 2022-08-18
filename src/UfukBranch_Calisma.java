6961897d7e08db252e1d37f98464f1f9da0159f8
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-08-18 18:47:11 +0200

    Catismabeklenen2

--- src/UfukBranch_Calisma.java
+++ src/UfukBranch_Calisma.java
@@ -1,5 +1,8 @@
 public class UfukBranch_Calisma {
     public static void main(String[] args) {
         System.out.println("Branchte calisma 1");
+
+
+        System.out.println(" Ufuk branch i catisma beklenen ");
     }
 }
